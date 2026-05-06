18498a9cc8206c9a7143569c4e5780c569541ba2
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-06 12:59:08 +0530

    Product Entity added, AppDbContext Added and made Database connection by adding into program.cs

---
+++ Ecommerce.API/Ecommerce.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
