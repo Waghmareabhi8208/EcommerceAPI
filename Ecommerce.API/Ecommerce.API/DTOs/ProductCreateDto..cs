1089db4a530c6746a93ab95d281e1892a401a660
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-06 13:47:56 +0530

    DTOs folder created and ProductCreateDto.cs and ProductResponseDto.cs added in it for better control of What data enters/leaves API

---
+++ Ecommerce.API/Ecommerce.API/DTOs/ProductCreateDto..cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.API.DTOs
+{
+    public class ProductCreateDto
+    {
+        public string Name {  get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price {  get; set; }
+
+        public int Stock { get; set; }
+    }
+}
