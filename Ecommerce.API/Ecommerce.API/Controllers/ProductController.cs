08d2da7f776aa943925114247a2c076501ebee67
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-06 13:26:48 +0530

    GetApi for all products and PostApi for adding Product added in ProductController

---
+++ Ecommerce.API/Ecommerce.API/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Gives the list of All products
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+    }
+}
