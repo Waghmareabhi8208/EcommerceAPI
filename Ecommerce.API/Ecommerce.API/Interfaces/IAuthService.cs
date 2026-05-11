1ade2f993275d554c1da095b0c834be418270169
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-11 12:06:32 +0530

    Added Token Validation Parameters

---
+++ Ecommerce.API/Ecommerce.API/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.API.DTOs.Auth;
+
+namespace Ecommerce.API.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto dto);
+
+        Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    }
+}
--- Ecommerce.API/Ecommerce.API/Program.cs
+++ Ecommerce.API/Ecommerce.API/Program.cs
@@ -4,8 +4,11 @@ using Ecommerce.API.Repositories;
 using Ecommerce.API.Services;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +34,25 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
+// Adding Token Validation Parameters using JwtBearer
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,6 +63,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Middleware Added for authentication
+app.UseAuthentication();
 
 app.UseAuthorization();
 
