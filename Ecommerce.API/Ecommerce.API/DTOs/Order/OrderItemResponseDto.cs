021b1d400cc3a5e508359fda1862068774880a9f
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-13 16:41:17 +0530

    OrderItemResponseDto and OrderResponseDto created

---
+++ Ecommerce.API/Ecommerce.API/DTOs/Order/OrderItemResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.API.DTOs.Order
+{
+    public class OrderItemResponseDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Ecommerce.API/Ecommerce.API/DTOs/Order/OrderResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.API.DTOs.Order
+{
+    public class OrderResponseDto
+    {
+        public int OrderId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        public List<OrderItemResponseDto> Items { get; set; } = [];
+    }
+}
