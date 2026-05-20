55251d4e6bc850f802e2668eb0852bcb760bb13f
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-20 13:47:58 +0530

    VerifyPaymentDto created

---
+++ Ecommerce.API/Ecommerce.API/DTOs/Payment/VerifyPaymentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.API.DTOs.Payment
+{
+    public class VerifyPaymentDto
+    {
+        public int OrderId { get; set; }
+        public string RazorpayPaymentId { get; set; } = string.Empty;
+        public string RazorpayOrderId { get; set; } = string.Empty;
+        public string RazorpaySignature { get; set; } = string.Empty;
+    }
+}
