cb6503d7ca2431dde62b93abc6c6841bca7c0d52
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-05-26 13:09:44 +0530

    Validations added for login

---
+++ Ecommerce.API/Ecommerce.API/Validators/LoginDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.API.DTOs.Auth;
+using FluentValidation;
+
+namespace Ecommerce.API.Validators
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+
+                .EmailAddress()
+                .WithMessage("Invalid email format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required");
+        }
+    }
+}
--- Ecommerce.API/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
+++ Ecommerce.API/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac00784998075fc0da0ae7b6a9ea3832a77b9160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+161e072c96fd8bfbd468d0d672142fa3a3501a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
