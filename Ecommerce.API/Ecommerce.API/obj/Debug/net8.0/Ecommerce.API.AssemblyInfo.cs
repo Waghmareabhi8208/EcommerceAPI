725f5cf6c2ec0eaace002fc2dcec1f2591fe2fee
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-06-02 13:58:46 +0530

    wwwroot folder added and upload image api executed and image savedin wwwroot folder

--- Ecommerce.API/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
+++ Ecommerce.API/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e072bb0973757026aad3017cabab899ec2dc8a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b058e8cacaba6520568986f38f8c11015163ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
